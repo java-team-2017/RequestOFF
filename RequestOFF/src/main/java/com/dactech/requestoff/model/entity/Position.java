f2cb9b244b43d971981c83618e3fb86388dc7d9e
Cao Hai Nhi <nhicao1995@gmail.com>
2018-01-09 09:32:25 +0700

    Add Position model

---
+++ RequestOFF/src/main/java/com/dactech/requestoff/model/entity/Position.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package com.dactech.requestoff.model.entity;
+
+/**
+ * @author Nhi Cao
+ *
+ */
+public class Position {
+	private long id;
+	private String name;
+	private int validFlag;
+	private String insertDate;
+	private long insertOperator;
+	private String updateDate;
+	private long updateOperator;
+
+	public Position() {
+	}
+
+	public Position(long id, String name, int validFlag, String insertDate, long insertOperator, String updateDate,
+			long updateOperator) {
+		this.id = id;
+		this.name = name;
+		this.validFlag = validFlag;
+		this.insertDate = insertDate;
+		this.insertOperator = insertOperator;
+		this.updateDate = updateDate;
+		this.updateOperator = updateOperator;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getValidFlag() {
+		return validFlag;
+	}
+
+	public void setValidFlag(int validFlag) {
+		this.validFlag = validFlag;
+	}
+
+	public String getInsertDate() {
+		return insertDate;
+	}
+
+	public void setInsertDate(String insertDate) {
+		this.insertDate = insertDate;
+	}
+
+	public long getInsertOperator() {
+		return insertOperator;
+	}
+
+	public void setInsertOperator(long insertOperator) {
+		this.insertOperator = insertOperator;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public long getUpdateOperator() {
+		return updateOperator;
+	}
+
+	public void setUpdateOperator(long updateOperator) {
+		this.updateOperator = updateOperator;
+	}
+
+}
