08cd9e68c5e715c445713d1ba06ceddf4425dae6
Binh Thien <tucothien@gmail.com>
2018-01-22 13:55:50 +0700

    # RO-10 Request search Api - edit controller

--- RequestOFF/src/main/java/com/dactech/requestoff/controller/RequestController.java
+++ RequestOFF/src/main/java/com/dactech/requestoff/controller/RequestController.java
@@ -6,19 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dactech.requestoff.model.common.StatusInfo;
 import com.dactech.requestoff.model.request.RequestDetailsRequest;
 import com.dactech.requestoff.model.request.RequestRegistRequest;
 import com.dactech.requestoff.model.request.RequestSearchRequest;
 import com.dactech.requestoff.model.response.RequestDetailsResponse;
 import com.dactech.requestoff.model.response.RequestRegistResponse;
 import com.dactech.requestoff.model.response.RequestSearchResponse;
+import com.dactech.requestoff.model.response.TeamRegistResponse;
+import com.dactech.requestoff.model.response.TeamSearchResponse;
 import com.dactech.requestoff.service.RequestService;
 
 @RestController
 public class RequestController {
 	@Autowired
 	RequestService requestService;
-	
+
 	@RequestMapping(value = "/request/regist", method = RequestMethod.POST)
 	public RequestRegistResponse regist(@RequestBody RequestRegistRequest requestRegistRequest) {
 		return requestService.regist(requestRegistRequest);
@@ -26,9 +29,18 @@ public class RequestController {
 
 	@RequestMapping(value = "/request/search", method = RequestMethod.POST)
 	public RequestSearchResponse requestSearch(@RequestBody RequestSearchRequest requestSearchRequest) {
-		return requestService.requestSearch(requestSearchRequest);
+		RequestSearchResponse response = new RequestSearchResponse();
+		try {
+			response = requestService.requestSearch(requestSearchRequest);
+			response.setStatusInfo(new StatusInfo(StatusInfo.SUCCESS, null));
+		} catch (Exception e) {
+			response = new RequestSearchResponse();
+			response.setStatusInfo(new StatusInfo(StatusInfo.ERROR, e.getMessage()));
+		}
+
+		return response;
 	}
-	
+
 	@RequestMapping(value = "/request/details", method = RequestMethod.POST)
 	public RequestDetailsResponse details(@RequestBody RequestDetailsRequest requestDetailsRequest) {
 		return requestService.details(requestDetailsRequest);
