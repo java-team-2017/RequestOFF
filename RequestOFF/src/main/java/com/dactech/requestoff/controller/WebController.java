9fb6d952c6e826c57b944c5854852536a15e2873
Cao Hai Nhi <nhicao1995@gmail.com>
2018-02-01 14:52:28 +0700

    refactor Web controller

--- RequestOFF/src/main/java/com/dactech/requestoff/controller/WebController.java
+++ RequestOFF/src/main/java/com/dactech/requestoff/controller/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class DefaultController {
+public class WebController {
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
 		return "/index";
