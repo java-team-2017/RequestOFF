0c121a2735808c58cd9ccffda38140bc4adf3b4e
Minh Hien <minhhienphamcntt@gmail.com>
2018-03-27 14:42:52 +0700

    #refactor

--- RequestOFF/src/main/java/com/dactech/requestoff/service/impl/SlackRequestServiceImpl.java
+++ RequestOFF/src/main/java/com/dactech/requestoff/service/impl/SlackRequestServiceImpl.java
@@ -334,6 +334,8 @@ public class SlackRequestServiceImpl implements SlackRequestService {
 
 	@Override
 	public void sendRequestMsgToSlack(Request request) throws Exception{
+		Employee recipient = employeeRepository.findById(request.getRecipientId());
+		request.setRecipientName(recipient.getName());
 		String url = "https://slack.com/api/chat.postMessage";
 		String token = "xoxp-282152434071-281018004115-334900370514-b001a2686c8d3f22feabab9592703ec5";
 		String channel = "C94BSF83C";
